1c51c87a691369db6996b1959d2f33c69dbb3f59
Nihara Bamunusinghe <akasuninihara@gmail.com>
2023-08-19 01:53:47 +0530

    Complete payable tax

--- taxCal.java
+++ taxCal.java
@@ -17,7 +17,8 @@ public class taxCal {
                 //System.out.println("Choose 1");
                 break;
             case 2:
-                System.out.println("Choose 2");
+                PayableTax();
+                //System.out.println("Choose 2");
                 break;
             case 3:
                 System.out.println("Choose 3");
@@ -76,6 +77,46 @@ public class taxCal {
         }
         else{
             System.out.println("\tYou do not have to pay rent tax...");
+        }
+    }
+    
+    public static void PayableTax(){
+        System.out.print("Enter monthly income : ");
+        int income = sc.nextInt();
+        double tax = 0;
+        double taxRate = 0;
+        if(income <= 100000){
+            System.out.println("No tax");
+        }
+        else if(income <= 141667){
+            taxRate = 0.06;
+            tax = ( (income - 1416667)*taxRate );
+            System.out.println("Tax : "+Math.round(tax));
+        }
+        else if(income <= 183333){
+            taxRate = 0.12;
+            tax = ( (income - 1416667)*taxRate ) + 2500;
+            System.out.println("Tax : "+Math.round(tax));
+        }
+        else if(income <= 225000){
+            taxRate = 0.18;
+            tax = ( (income - 183333)*taxRate ) + 7500;
+            System.out.println("Tax : "+Math.round(tax));
+        }
+        else if(income <= 266667){
+            taxRate = 0.24;
+            tax = ( (income - 225000)*taxRate ) + 15000;
+            System.out.println("Tax : "+Math.round(tax));
+        }
+        else if(income <= 308333){
+            taxRate = 0.3;
+            tax = ( (income - 266667)*taxRate ) + 25000;
+            System.out.println("Tax : "+Math.round(tax));
+        }
+        else{
+            taxRate = 0.36;
+            tax = ( (income - 308333)*taxRate ) + 37500;
+            System.out.println("Tax : "+Math.round(tax));
         }
     }
 }
